32a5e13d78f69e7df7ee3de00662560b1c66a454
Lirone29 <kalata.eliza@gmail.com>
2019-05-27 00:15:19 +0200

    Initial commit

---
+++ library/goodMorning.py
@@ -0,0 +1,15 @@
+def goodMorning():
+    print("Good Morning!")
+
+def goodMorningInPolish():
+    print("Dzień Dobry")
+
+def goodMorningInFrench():
+    print("Bonjour!")
+
+def goodMorningInKorean():
+    for i in range(0, 3):
+        print("좋은 아침입니다")
+
+def goodMorningTo(name):
+    print("Good Morning" + name + " !!")
---
+++ library/sayCheeseLib.py
@@ -0,0 +1,12 @@
+def sayCheeseLib():
+    print("Saying Cheese")
+
+def sayCheeseCheeseLib():
+    print("Saying Cheese")
+
+def sayCheeseXTimes(number):
+    for x in range(number):
+        print("Cheese")
+
+def sayGoodbyeToCheese():
+    print("GoodBye <3")
