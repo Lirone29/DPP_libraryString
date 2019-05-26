8210457754148c3238f1d49a85a664fabfd61f11
Lirone29 <kalata.eliza@gmail.com>
2019-05-27 01:44:22 +0200

    The rest of files

---
+++ makeWordsFunny/__init__.py
@@ -0,0 +1,2 @@
+from . import goodMorning
+from . import sayCheeseLib
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='makeWordsFunny',
+    version='1.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Library for DPP lab 10',
+    long_description=open('README.md').read(),
+    install_requires=[''],
+    url='https://github.com/Lirone29/DPP_libPython',
+    author='Eliza Kalata',
+    author_email='kalata.eliza@gmail.com'
+)
